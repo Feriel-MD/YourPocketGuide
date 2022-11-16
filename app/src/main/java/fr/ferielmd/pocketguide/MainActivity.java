df693582531f42ddec84e1df7527ef5c369874a9
Feriel-MD <feriel.mokrani@edu.esiee.fr>
2022-11-16 16:19:30 +0100

    first Commit

---
+++ app/src/main/java/fr/ferielmd/pocketguide/MainActivity.java
@@ -0,0 +1,30 @@
+package fr.ferielmd.pocketguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.open_button);
+        b.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.open_button:
+                startActivity(new Intent(MainActivity.this, transaction.class));
+                //finish();
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/fr/ferielmd/pocketguide/transaction.java
@@ -0,0 +1,14 @@
+package fr.ferielmd.pocketguide;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class transaction extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.transaction);
+    }
+
+}
