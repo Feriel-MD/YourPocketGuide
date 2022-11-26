f5bc3e005b49d7c9c8140d8c2d657cd83ad22549
Feriel-MD <feriel.mokrani@edu.esiee.fr>
2022-11-26 16:45:42 +0100

    Fourth Commit

--- app/src/main/java/fr/ferielmd/pocketguide/RecyclerviewRestaurents.java
+++ app/src/main/java/fr/ferielmd/pocketguide/RecyclerviewRestaurents.java
@@ -20,10 +20,10 @@ public class RecyclerviewRestaurents extends AppCompatActivity {
 
         List<Item> items = new ArrayList<>();
         items.add(new Item("Benidict","19 Rue Sainte-Croix de la Bretonnerie, 75004 Paris","Téléphone : 01 42 76 91 37",R.drawable.benedict));
-        items.add(new Item("I GRAPPOLI","22 Pl. Denfert-Rochereau, 75014 Paris","Téléphone : 01 42 76 91 37",R.drawable.i_grappoli));
-        items.add(new Item("PATROL - PARIS","2 Pl. Edmond Rostand, 75006 Paris","Téléphone : 01 42 76 91 37",R.drawable.patrol_paris));
-        items.add(new Item("ORA - LA CASERNE","12 Rue Philippe de Girard, 75010 Paris","Téléphone : 01 42 76 91 37",R.drawable.ora_lacaserne));
-        items.add(new Item("Neni Paris","12 Bd de Denain, 75010 Paris","Téléphone : 01 42 76 91 37",R.drawable.neni_france));
+        items.add(new Item("I GRAPPOLI","22 Pl. Denfert-Rochereau, 75014 Paris","Téléphone : 01 43 21 22 59",R.drawable.i_grappoli));
+        items.add(new Item("PATROL - PARIS","2 Pl. Edmond Rostand, 75006 Paris","Téléphone : 01 53 19 82 70",R.drawable.patrol_paris));
+        items.add(new Item("ORA - LA CASERNE","12 Rue Philippe de Girard, 75010 Paris","Téléphone : 06 75 70 59 72",R.drawable.ora_lacaserne));
+        items.add(new Item("Neni Paris","12 Bd de Denain, 75010 Paris","Téléphone : 01 42 80 20 00",R.drawable.neni_france));
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new MyAdapter(getApplicationContext(),items));
